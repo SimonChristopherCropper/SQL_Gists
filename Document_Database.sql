3de6f53b019ea76287dc28f3f384fe694f5018ac
Simon Cropper <simon.cropper@bupa.com.au>
2019-04-16 11:33:06 +1000

    Test commit

--- Document_Database.sql
+++ Document_Database.sql
@@ -1,5 +1,5 @@
-/* This allows you to list all the views for a particular {owner} */
+/* This allows you to list all the views for a particular {owner} in the database */
 SELECT * FROM ALL_VIEWS where owner='{owner}';
 
-/* This allows you to list all the columns in the views/tables for a particular {owner} */
+/* This allows you to list all the columns in the views/tables for a particular {owner} in the database */
 SELECT * FROM ALL_TAB_COLS where owner='{owner}';
