98ea1acf0e06e8dc0667fef0226fbc77d07ad07f
Simon Cropper <>
2019-04-16 10:10:19 +1000

    Commented and generalised the script

--- Document_Database.sql
+++ Document_Database.sql
@@ -1,2 +1,5 @@
-SELECT * FROM ALL_TAB_COLS where owner='SVOP';
-SELECT * FROM ALL_VIEWS where owner='SVOP'
+/* This select allows you to list all the views for a particular {owner} */
+SELECT * FROM ALL_VIEWS where owner='{owner}'
+
+/* This allows you to list all the columns in the views/tables for a particular {owner} */
+SELECT * FROM ALL_TAB_COLS where owner='{owner}';
